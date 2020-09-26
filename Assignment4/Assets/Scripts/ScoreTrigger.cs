2b89fcc5517ad508e00eb4d5aa1c5b814b71deb4
Frank Calabrese <57340460+FrankCalabrese@users.noreply.github.com>
2020-09-26 16:14:43 -0500

    prototype 1 part 1

---
+++ Assignment4/Assets/Scripts/ScoreTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTrigger : MonoBehaviour
+{
+    private UIManager uIManager;
+
+    private bool triggered = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uIManager = GameObject.FindObjectOfType<UIManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && !triggered)
+        {
+            triggered = true;
+            uIManager.score++;
+        }
+    }
+}
